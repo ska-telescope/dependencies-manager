0709886a9ecf03aca119dd5a3e9a0be2c9d766fe
Bruno S. Morgado <jb.morgado@gmail.com>
2019-01-22 15:24:17 +0000

    add developer.skatelescope.org  repo

--- get-deps.sh
+++ get-deps.sh
@@ -21,9 +21,9 @@ working_dir=${ska_python_skeleton_dir}
 working_url=${ska_python_skeleton_url}
 
 ## get dependencies
-echo "[+] Parsing ska-python-skeleton project"
+echo "[+] Parsing ${working_dir} project"
 rm -rf ${PWD}/dependencies/${working_dir}
-mkdir -p ${PWD}/dependencies/${working_dir};
+mkdir -p ${PWD}/dependencies/${working_dir}
 docker run --name python_bash -v ${PWD}/dependencies/${working_dir}:/tmp/dependencies/ -v ${PWD}/.utils/:/tmp/utils/ --rm -t spsr /bin/bash -c \
     "rm -rf /tmp/repo && \
     git clone ${working_url} /tmp/repo && \
@@ -39,25 +39,24 @@ diff ${PWD}/dependencies/base/system_deps.csv ${PWD}/dependencies/${working_dir}
 diff ${PWD}/dependencies/base/lib_deps.txt ${PWD}/dependencies/${working_dir}/lib_deps.txt perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/lib_deps_diff.txt
 
 # DEVELOPER.SKA.TELESCOPE ---------------------------------------------------------------------------------------------------------------------------------
-# working_dir=${developer_ska_telescope_dir}
-# working_url=${developer_ska_telescope_url}
+working_dir=${developer_ska_telescope_dir}
+working_url=${developer_ska_telescope_url}
 
-# ## get dependencies
-# echo "[+] Parsing ska-python-skeleton project"
-# if [ ! -d ${PWD}/dependencies/${working_dir} ]; then
-#   mkdir -p ${PWD}/dependencies/${working_dir};
-# fi
-# docker run --name python_bash -v ${PWD}/dependencies/${working_dir}:/tmp/repo/dependencies/ -v ${PWD}/.docker/:/tmp/repo/.docker/ --rm -t spsr /bin/bash -c \
-#     "cd /tmp && git clone ${working_url} repotmp && \
-#     cp -R repotmp/* repo/ && \
-#     cd repo && \
-#     pipenv install && \
-#     make html && \
-#     /tmp/utils/get-lib_deps.sh && \
-#     /tmp/utils/get-pipenv_deps.sh && \
-#     /tmp/utils/get-system_deps.sh"
+## get dependencies
+echo "[+] Parsing ${working_dir} project"
+rm -rf ${PWD}/dependencies/${working_dir}
+mkdir -p ${PWD}/dependencies/${working_dir}
+docker run --name python_bash -v ${PWD}/dependencies/${working_dir}:/tmp/dependencies/ -v ${PWD}/.utils/:/tmp/utils/ --rm -t spsr /bin/bash -c \
+    "rm -rf /tmp/repo && \
+    git clone ${working_url} /tmp/repo && \
+    cd /tmp/repo && \
+    pipenv install && \
+    pipenv run make html && \
+    /tmp/utils/get-lib_deps.sh && \
+    /tmp/utils/get-pipenv_deps.sh && \
+    /tmp/utils/get-system_deps.sh"
 
-# ## get installed dependencies on top of base
-# diff ${PWD}/dependencies/base/system_deps.txt ${PWD}/dependencies/${working_dir}/system_deps.txt | perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/system_deps_diff.txt
-# diff ${PWD}/dependencies/base/system_deps.csv ${PWD}/dependencies/${working_dir}/system_deps.csv perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/system_deps_diff.csv
-# diff ${PWD}/dependencies/base/lib_deps.txt ${PWD}/dependencies/${working_dir}/lib_deps.txt perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/lib_deps_diff.txt
+## get installed dependencies on top of base
+diff ${PWD}/dependencies/base/system_deps.txt ${PWD}/dependencies/${working_dir}/system_deps.txt | perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/system_deps_diff.txt
+diff ${PWD}/dependencies/base/system_deps.csv ${PWD}/dependencies/${working_dir}/system_deps.csv perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/system_deps_diff.csv
+diff ${PWD}/dependencies/base/lib_deps.txt ${PWD}/dependencies/${working_dir}/lib_deps.txt perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/lib_deps_diff.txt
