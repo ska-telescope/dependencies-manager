4249bd7c80ab5eb3ba70083bf46e4da8d830bea2
Bruno S. Morgado <jb.morgado@gmail.com>
2019-02-05 18:21:14 +0000

    added placeholders to make it easier to edit

--- get-deps.sh
+++ get-deps.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
-ska_python_skeleton_dir="ska-skeleton"
+# PROJECT_dir="" # define the directory to put the products on (it should be the project name) 
+# PROJECT_REPO_url=" # define the git repo from where to clone to project
+ska_python_skeleton_dir="ska-skeleton" 
 ska_python_skeleton_url="https://github.com/ska-telescope/ska-skeleton.git"
 developer_ska_telescope_dir="developer.skatelescope.org"
 developer_ska_telescope_url="https://github.com/ska-telescope/developer.skatelescope.org.git"
@@ -15,6 +17,29 @@ docker run --name python_bash -v ${PWD}/dependencies/base:/tmp/dependencies/ -v 
     cd /tmp/repo && \
     /tmp/utils/get-lib_deps.sh && \
     /tmp/utils/get-system_deps.sh"
+
+# PROJECT -----------------------------------------------------------------------------------------------------------------------------------
+# working_dir=${PROJECT_dir}
+# working_url=${PROJECT_REPO_url}
+
+# ## get dependencies
+# echo "[+] Parsing ${working_dir} project"
+# rm -rf ${PWD}/dependencies/${working_dir}
+# mkdir -p ${PWD}/dependencies/${working_dir}
+# docker run --name python_bash -v ${PWD}/dependencies/${working_dir}:/tmp/dependencies/ -v ${PWD}/.utils/:/tmp/utils/ --rm -t spsr /bin/bash -c \
+#     "rm -rf /tmp/repo && \
+#     git clone ${working_url} /tmp/repo && \
+#     cd /tmp/repo && \
+#     pipenv install && \
+#     /tmp/utils/get-lib_deps.sh && \
+#     /tmp/utils/get-pipenv_deps.sh && \
+#     /tmp/utils/get-system_deps.sh"
+
+# ## get installed dependencies on top of base
+# diff ${PWD}/dependencies/base/system_deps.txt ${PWD}/dependencies/${working_dir}/system_deps.txt | perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/system_deps_diff.txt
+# echo "package, version" > ${PWD}/dependencies/${working_dir}/system_deps_diff.csv
+# diff ${PWD}/dependencies/base/system_deps.csv ${PWD}/dependencies/${working_dir}/system_deps.csv perl -lne 'if(/^[<>]/){s/^..//g;print}' >> ${PWD}/dependencies/${working_dir}/system_deps_diff.csv
+# diff ${PWD}/dependencies/base/lib_deps.txt ${PWD}/dependencies/${working_dir}/lib_deps.txt perl -lne 'if(/^[<>]/){s/^..//g;print}' > ${PWD}/dependencies/${working_dir}/lib_deps_diff.txt
 
 # SKA-PYTHON-SKELETON -----------------------------------------------------------------------------------------------------------------------------------
 working_dir=${ska_python_skeleton_dir}
